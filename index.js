aa476fa85027c65dc2cc0d0a9de923e53c8c8069
Israe Moubayed <israetarek@yahoo.com>
2021-05-28 14:55:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // Your code here
 
+function moveDodgerRight() {
+  let RightNumbers = dodger.style.right.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
+ 
+  if (right > 0) {
+    dodger.style.right = `${right + 1}px`;
+  }
+}
+
 document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
   }
 });
