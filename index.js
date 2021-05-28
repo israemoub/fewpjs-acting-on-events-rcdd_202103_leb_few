ceb0e0e89d5fad8e590e8acd2ee226bed70fc636
Israe Moubayed <israetarek@yahoo.com>
2021-05-28 15:06:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ let dodger = document.getElementById("dodger");
 
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
+  let rightNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(rightNumbers, 10);
  
   if (right > 0) {
-    dodger.style.right = `${right + 1}px`;
+    dodger.style.left = `${left + 1}px`;
   }
 }
 
