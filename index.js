f1e2ca4b6d758ecb371a6aeb394794c52caa0e41
Israe Moubayed <israetarek@yahoo.com>
2021-05-28 15:10:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,14 @@ document.addEventListener("keydown", function(e) {
     moveDodgerRight();
   }
 });
+
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
